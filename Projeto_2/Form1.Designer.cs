5235a7044be0a032088214daa90799151b2c53a9
joaoteixeira_ <jp2887860@gmail.com>
2025-01-29 17:21:22 -0300

    Update

--- Projeto_2/Form1.Designer.cs
+++ Projeto_2/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cbxAtFisica = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,25 +39,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tempoEx)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox1.Image = global::Projeto_2.Properties.Resources.teste1;
-            this.pictureBox1.Location = new System.Drawing.Point(419, -20);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(380, 466);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 81);
+            this.label1.Location = new System.Drawing.Point(180, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(219, 16);
             this.label1.TabIndex = 1;
@@ -66,6 +55,9 @@
             // 
             // cbxAtFisica
             // 
+            this.cbxAtFisica.BackColor = System.Drawing.SystemColors.Control;
+            this.cbxAtFisica.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxAtFisica.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbxAtFisica.FormattingEnabled = true;
             this.cbxAtFisica.Items.AddRange(new object[] {
             "Caminhar",
@@ -74,16 +66,18 @@
             "Tênis",
             "Dança",
             "Alongamento"});
-            this.cbxAtFisica.Location = new System.Drawing.Point(247, 80);
+            this.cbxAtFisica.Location = new System.Drawing.Point(405, 51);
             this.cbxAtFisica.Name = "cbxAtFisica";
             this.cbxAtFisica.Size = new System.Drawing.Size(166, 21);
             this.cbxAtFisica.TabIndex = 2;
+            this.cbxAtFisica.SelectedIndexChanged += new System.EventHandler(this.cbxAtFisica_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 121);
+            this.label2.Location = new System.Drawing.Point(180, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(156, 16);
             this.label2.TabIndex = 3;
@@ -91,16 +85,21 @@
             // 
             // txtPeso
             // 
-            this.txtPeso.Location = new System.Drawing.Point(174, 120);
+            this.txtPeso.BackColor = System.Drawing.SystemColors.Control;
+            this.txtPeso.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPeso.ForeColor = System.Drawing.Color.Black;
+            this.txtPeso.Location = new System.Drawing.Point(344, 93);
             this.txtPeso.Name = "txtPeso";
-            this.txtPeso.Size = new System.Drawing.Size(57, 20);
+            this.txtPeso.Size = new System.Drawing.Size(227, 20);
             this.txtPeso.TabIndex = 4;
+            this.txtPeso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPeso_KeyPress);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 163);
+            this.label3.Location = new System.Drawing.Point(180, 132);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(245, 16);
             this.label3.TabIndex = 5;
@@ -108,16 +107,33 @@
             // 
             // tempoEx
             // 
-            this.tempoEx.Location = new System.Drawing.Point(263, 163);
+            this.tempoEx.BackColor = System.Drawing.SystemColors.Control;
+            this.tempoEx.Location = new System.Drawing.Point(431, 132);
+            this.tempoEx.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.tempoEx.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.tempoEx.Name = "tempoEx";
             this.tempoEx.Size = new System.Drawing.Size(47, 20);
             this.tempoEx.TabIndex = 6;
+            this.tempoEx.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 258);
+            this.label4.Location = new System.Drawing.Point(180, 258);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(149, 16);
             this.label4.TabIndex = 7;
@@ -125,9 +141,12 @@
             // 
             // resKcal
             // 
-            this.resKcal.Location = new System.Drawing.Point(167, 254);
+            this.resKcal.BackColor = System.Drawing.SystemColors.Control;
+            this.resKcal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resKcal.ForeColor = System.Drawing.Color.Black;
+            this.resKcal.Location = new System.Drawing.Point(335, 258);
             this.resKcal.Name = "resKcal";
-            this.resKcal.Size = new System.Drawing.Size(123, 20);
+            this.resKcal.Size = new System.Drawing.Size(48, 20);
             this.resKcal.TabIndex = 8;
             // 
             // button1
@@ -137,7 +156,7 @@
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
             this.button1.Location = new System.Drawing.Point(15, 322);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(76, 36);
             this.button1.TabIndex = 9;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = false;
@@ -148,30 +167,33 @@
             this.button2.BackColor = System.Drawing.Color.Gray;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.SystemColors.Control;
-            this.button2.Location = new System.Drawing.Point(174, 322);
+            this.button2.Location = new System.Drawing.Point(137, 322);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 36);
             this.button2.TabIndex = 10;
             this.button2.Text = "Limpar";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Firebrick;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.ForeColor = System.Drawing.SystemColors.Control;
-            this.button3.Location = new System.Drawing.Point(338, 322);
+            this.button3.Location = new System.Drawing.Point(713, 322);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(75, 36);
             this.button3.TabIndex = 11;
             this.button3.Text = "Sair";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Kcal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BackgroundImage = global::Projeto_2.Properties.Resources.teste;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(800, 370);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -184,10 +206,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cbxAtFisica);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
             this.Name = "Kcal";
             this.Text = "Calcule seu gasto de calorias";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tempoEx)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -195,8 +216,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbxAtFisica;
         private System.Windows.Forms.Label label2;
--- Projeto_2/Form1.cs
+++ Projeto_2/Form1.cs
@@ -19,10 +19,93 @@ namespace Projeto_2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double peso = Convert.ToDouble(txtPeso.Text);
-            int tempoExer = Convert.ToInt32(tempoEx.Text);
+            if (cbxAtFisica.SelectedIndex.Equals(-1))
+            {
+                MessageBox.Show("Insira uma opção válida de atividade física!", "Atividade inválida!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                resKcal.Clear();
+                cbxAtFisica.Focus();
+            }
+            else if (txtPeso.Text == "")
+            {
+                MessageBox.Show("Insira uma opção válida para seu peso", "Peso indefinido!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                resKcal.Clear();
+                txtPeso.Focus();
+            }
+            else if (Convert.ToInt32(txtPeso.Text) < 1 || Convert.ToInt32(txtPeso.Text) > 200)
+            {
+                MessageBox.Show("Peso deve estar entre 1Kg e 200Kg", "Peso inválido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPeso.Clear();
+                resKcal.Clear();
+                txtPeso.Focus();
+            }
+            /*
+            Caminhar
+            Musculação
+            Futebol
+            Tênis
+            Dança
+            Alongamento
+            */
+            else
+            {
+                switch (cbxAtFisica.SelectedItem)
+                {
+                    case "Caminhar":
+                        resKcal.Text = (Convert.ToDecimal(txtPeso.Text) * tempoEx.Value * 1).ToString();
+                    break;
 
-            string kcal = 
+                    case "Musculação":
+                        resKcal.Text = (Convert.ToDecimal(txtPeso.Text) * tempoEx.Value * 2).ToString(); 
+                    break;
+
+                    case "Futebol":
+                        resKcal.Text = (Convert.ToDecimal(txtPeso.Text) * tempoEx.Value * 3).ToString();
+                    break;
+
+                    case "Tênis":
+                        resKcal.Text = (Convert.ToDecimal(txtPeso.Text) * tempoEx.Value * 2).ToString();
+                    break;
+
+                    case "Dança":
+                        resKcal.Text = (Convert.ToDecimal(txtPeso.Text) * tempoEx.Value * 1).ToString();
+                    break;
+
+                    case "Alongamento":
+                        resKcal.Text = (Convert.ToDecimal(txtPeso.Text) * tempoEx.Value * 5).ToString();
+                    break;
+
+                    default:
+                        MessageBox.Show("Escolha uma opção válida!");
+                    break;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtPeso.Clear();
+            resKcal.Clear();
+            tempoEx.Value = 1;
+            cbxAtFisica.SelectedItem = null;
+            cbxAtFisica.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cbxAtFisica_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            resKcal.Clear();
+        }
+
+        private void txtPeso_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(char.IsLetter(e.KeyChar) || char.IsSymbol(e.KeyChar) || char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
